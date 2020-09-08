9aced47908fc720824380232ee4b24268a4fbf88
Bona Brian Siagian <bonabriansiagian@gmail.com>
2020-09-08 12:29:17 +0700

    Add Homepage

---
+++ src/containers/HomePage/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="homepage">
+        <h3>Home Page App</h3>
+      </div>
+    );
+  }
+};
