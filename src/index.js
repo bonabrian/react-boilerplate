e2166c5ccc3d185f148e459b4dab47d7e4fb63d1
Bona Brian Siagian <bonabriansiagian@gmail.com>
2020-09-08 13:19:34 +0700

    Add React Helmet

--- src/index.js
+++ src/index.js
@@ -1,15 +1,41 @@
-import React from 'react';
+/**
+ * index.js
+ *
+ * This is entry file for the application.
+ */
+
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import { HelmetProvider } from 'react-helmet-async';
+
+const MOUNT_NODE = document.getElementById('root');
+
+/* Root component */
+const RootApp = ({ Component }) => (
+  <HelmetProvider>
+    <React.StrictMode>
+      <Component />
+    </React.StrictMode>
+  </HelmetProvider>
 );
+
+const render = (Component) => {
+  ReactDOM.render(<RootApp Component={Component} />, MOUNT_NODE);
+};
+
+if (module.hot) {
+  module.hot.accept(['./App'], () => {
+    ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+    const App = require('./App').default;
+    render(App);
+  });
+}
+
+render(App);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
